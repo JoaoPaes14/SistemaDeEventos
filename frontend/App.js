3b056f25e815b2410eca67fce28bbb31d822fb04
João Batista Paes <joaopaes556@gmail.com>
2024-12-16 17:07:36 -0300

    Terminando as configs

---
+++ frontend/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { SafeAreaView, Text, StyleSheet } from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>Bem-vindo ao meu App!</Text>
+    </SafeAreaView>
+  );
+};
+export default App;
